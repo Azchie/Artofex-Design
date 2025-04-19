e8c9f8a0828531c8d297df754dd2deab470bcddd
Azchie <156080140+Azchie@users.noreply.github.com>
2025-04-19 17:59:09 +0100

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', () => {
-    const header = document.querySelector('.header-main');
-    if (window.scrollY > 10) {
-        header.classList.add('shrink');
-    } else {
-        header.classList.remove('shrink');
-    }
-});
